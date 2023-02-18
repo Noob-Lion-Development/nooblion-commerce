9556d519cfef6122a73d86e612c955a640a8d5a0
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-18 15:11:03 -0500

    feature: add product card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { NavComponent } from 'src/components/nav/nav.component';
 import { FooterComponent } from 'src/components/footer/footer.component';
 import { TitleComponent } from 'src/components/title/title.component';
+import { ProductCardComponent } from 'src/components/product-card/product-card.component';
 
 @Component({
     selector: 'nbl-app',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, NavComponent, TitleComponent, FooterComponent],
+    imports: [CommonModule, RouterOutlet, NavComponent, TitleComponent, FooterComponent, ProductCardComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
---
+++ src/components/product-card/product-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nbl-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+
+}
