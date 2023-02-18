8ffa0713fd0cfc5870c96f6e455de4bdd31e2701
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-18 11:18:06 -0500

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from 'src/components/nav/nav.component';
+import { FooterComponent } from 'src/components/footer/footer.component';
 
 @Component({
     selector: 'nbl-app',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, NavComponent],
+    imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
