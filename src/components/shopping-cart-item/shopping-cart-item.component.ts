9dcf2df3c3cef4d8d654d41099f3a9fec4e4f992
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-19 09:47:05 -0500

    feat: create shopping cart item component

---
+++ src/components/shopping-cart-item/shopping-cart-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nbl-shopping-cart-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-cart-item.component.html',
+  styleUrls: ['./shopping-cart-item.component.css']
+})
+export class ShoppingCartItemComponent {
+
+}
