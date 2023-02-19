07170ed0b6352ad14e1fde12c51f5c379d4393fb
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-18 21:45:20 -0500

    feat: add interface for product

---
+++ src/interfaces/productInterface.ts
@@ -0,0 +1,12 @@
+interface Product{
+    imageURL: string;
+    productName: string;
+    productDetails?: string; //optional property
+    productPrice: number;
+    shippingPrice: number;
+    feesPrice: number;
+}
+
+function findTotalValue(productObj: Product){
+    return productObj.productPrice + productObj.shippingPrice + productObj.feesPrice;
+}
