ed59bae774e30eef9aac6fdce6debe8b00d4bc14
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-19 08:52:51 -0500

    feat: add shopping cart service

--- src/components/product-card/product-card.component.ts
+++ src/components/product-card/product-card.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProductService } from 'src/services/product.service';
+import { Product } from 'src/interfaces/productInterface';
+import { ShoppingCartService } from 'src/services/shopping-cart.service';
+
 @Component({
   selector: 'nbl-product-card',
   standalone: true,
@@ -9,6 +13,25 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent {
+    /*
+    THERE NEEDS TO
+
+    constructor(private productService: ProductService){}
+
+    products: Product[] = [];
+
+    getProducts(): void{
+        this.products = this.productService.getProducts();
+    }
+
+    ngOnInit(): void {
+        this.getProducts();
+    }
+    */
+
+
+
+    @Input() product?: Product;
     @Input() imageURL = 'assets/default.png';
     @Input() productName = 'Product Name Not Found';
     @Input() productDetails = 'No Product Description';
@@ -16,4 +39,11 @@ export class ProductCardComponent {
     @Input() shippingPrice = 0.00;
     @Input() feesPrice = 0.00;
     @Input() totalPrice = this.productPrice + this.shippingPrice + this.feesPrice;
+
+    addToCart(){
+        const shoppingCartService = new ShoppingCartService();
+        // TODO: this.productName will need to be replaced with calling this.product.productName when the product list component is built
+        shoppingCartService.onAddToCartClicked(this.productName);
+    }
+
 }
