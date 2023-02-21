b2edb54b04c2d275c69104d95c9552abe7beefb9
Noel Abellanosa <abellnoel99@gmail.com>
2023-02-20 19:37:19 -0500

    test jira integration

---
+++ src/pages/product-details/product-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nbl-product-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+
+}
