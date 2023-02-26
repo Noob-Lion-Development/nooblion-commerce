c0e8853e1cfad2455b17206cedb0ca5b3233bfb9
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-26 09:05:28 -0500

    changed Order interface

--- src/interfaces/Order.ts
+++ src/interfaces/Order.ts
@@ -1,19 +1,19 @@
-import { Product } from 'src/interfaces/productInterface';
+import { ShoppingCart } from "./shoppingCartInterface";
 
 export interface Order{
     /*
-    * Notice the Order interface is NOT a one for one with teh Order table in our api.
+    * Notice the Order interface is NOT a one for one with the Order table in our api.
     * This is due to Orders being slightly complex and actually broken into two different tables for normalization
     * 1). We don't need an ID because Entity FW will handle that for us
-    * 2). We are adding a list of products here that will be added to the OrderItem table
+    * 2). We are adding a cart object here that will keep track of our list of products that will eventually be added to the OrderItem table in the DB
     */
 
-    // TODO: figure out how I am going to push the products to the table while knowing the order ID and quantity, may require restructuring interface
+    // TODO: figure out how I am going to push the products to the OrderItem table while knowing the order ID and quantity, may require restructuring shoppingCart interface
     OrderCompleted: Date;
     FirstName: string;
     LastName: string;
     Country: string;
     Street: string;
     City: string;
-    Products: Product[];
+    Cart: ShoppingCart;
 }
